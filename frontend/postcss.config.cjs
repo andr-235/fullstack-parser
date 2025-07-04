95ad93d346eddf15d69662c3fcc8bcbdbf60880f
Андрей Винс <andreyvins2015@yandex.ru>
2025-07-04 12:08:11 +1000

    fix: переместил postcss.config.cjs в папку frontend для корректной сборки Next.js

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss').Config} */
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': { config: './tailwind.config.js' },
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
