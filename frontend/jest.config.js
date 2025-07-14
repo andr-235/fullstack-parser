159206dd91af5e348ea41251c2c45b7d0df307f4
Андрей Винс <andreyvins2015@yandex.ru>
2025-07-14 15:34:52 +1000

    test(frontend): исправить алиасы и добавить моки для стабильности CI/CD

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,15 +10,9 @@ const customJestConfig = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/features/(.*)$': '<rootDir>/features/$1',
-    '^@/hooks/(.*)$': '<rootDir>/hooks/$1',
-    '^@/shared/(.*)$': '<rootDir>/shared/$1',
-    '^@/types/(.*)$': '<rootDir>/types/$1',
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$':
-      '<rootDir>/__mocks__/fileMock.js',
+    '^@/(.*)$': '<rootDir>/$1',
+    '^.+\\.(css|scss|sass)$': 'identity-obj-proxy',
+    '^.+\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
@@ -33,4 +27,37 @@ const customJestConfig = {
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = {
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '@next/font/(.*)': '<rootDir>/__mocks__/nextFontMock.js',
+    'next/font/(.*)': '<rootDir>/__mocks__/nextFontMock.js',
+    'server-only': '<rootDir>/__mocks__/empty.js',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/.next/'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+}
